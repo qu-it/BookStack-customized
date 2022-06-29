366b13ce7a286a15f424f6f73322ceecaabe8032
abdulsalam <abds.alblihi@qu.edu.sa>
2022-06-29 13:42:51 +0300

    add custom theme for QU

---
+++ themes/custom/lang/ar/activities.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
+return [
+
+    // Pages
+    'page_create'                 => 'تم إنشاء صفحة',
+    'page_create_notification'    => 'تم إنشاء الصفحة بنجاح',
+    'page_update'                 => 'تم تحديث الصفحة',
+    'page_update_notification'    => 'تم تحديث الصفحة بنجاح',
+    'page_delete'                 => 'تم حذف الصفحة',
+    'page_delete_notification'    => 'تم حذف الصفحة بنجاح',
+    'page_restore'                => 'تمت استعادة الصفحة',
+    'page_restore_notification'   => 'تمت استعادة الصفحة بنجاح',
+    'page_move'                   => 'تم نقل الصفحة',
+
+    // Chapters
+    'chapter_create'              => 'تم إنشاء فصل',
+    'chapter_create_notification' => 'تم إنشاء الفصل بنجاح',
+    'chapter_update'              => 'تم تحديث الفصل',
+    'chapter_update_notification' => 'تم تحديث الفصل بنجاح',
+    'chapter_delete'              => 'تم حذف الفصل',
+    'chapter_delete_notification' => 'تم حذف الفصل بنجاح',
+    'chapter_move'                => 'تم نقل الفصل',
+
+    // Books
+    'book_create'                 => 'تم إنشاء السياسة',
+    'book_create_notification'    => 'تم إنشاء السياسة بنجاح',
+    'book_create_from_chapter'              => 'converted chapter to book',
+    'book_create_from_chapter_notification' => 'Chapter successfully converted to a book',
+    'book_update'                 => 'تم تحديث السياسة',
+    'book_update_notification'    => 'تم تحديث السياسة بنجاح',
+    'book_delete'                 => 'تم حذف السياسة',
+    'book_delete_notification'    => 'تم حذف السياسة بنجاح',
+    'book_sort'                   => 'تم سرد السياسة',
+    'book_sort_notification'      => 'تم إعادة فرز السياسة بنجاح',
+
+    // Bookshelves
+    'bookshelf_create'            => 'تم إنشاء الجهة',
+    'bookshelf_create_notification'    => 'تم إنشاء الجهة بنجاح',
+    'bookshelf_create_from_book'    => 'converted book to bookshelf',
+    'bookshelf_create_from_book_notification'    => 'Book successfully converted to a shelf',
+    'bookshelf_update'                 => 'تم تحديث الجهة',
+    'bookshelf_update_notification'    => 'تم تحديث الجهة بنجاح',
+    'bookshelf_delete'                 => 'تم تحديث الجهة',
+    'bookshelf_delete_notification'    => 'تم حذف الجهة بنجاح',
+
+    // Favourites
+    'favourite_add_notification' => 'تم إضافة ":name" إلى المفضلة لديك',
+    'favourite_remove_notification' => 'تم إزالة ":name" من المفضلة لديك',
+
+    // MFA
+    'mfa_setup_method_notification' => 'تم تكوين طريقة متعددة العوامل بنجاح',
+    'mfa_remove_method_notification' => 'تمت إزالة طريقة متعددة العوامل بنجاح',
+
+    // Webhooks
+    'webhook_create' => 'تم إنشاء webhook',
+    'webhook_create_notification' => 'تم إنشاء Webhook بنجاح',
+    'webhook_update' => 'تم تحديث webhook',
+    'webhook_update_notification' => 'تم تحديث Webhook بنجاح',
+    'webhook_delete' => 'حذف webhook',
+    'webhook_delete_notification' => 'تم حذف Webhook بنجاح',
+
+    // Users
+    'user_update_notification' => 'تم تحديث المستخدم بنجاح',
+    'user_delete_notification' => 'تم إزالة المستخدم بنجاح',
+
+    // Other
+    'commented_on'                => 'تم التعليق',
+    'permissions_update'          => 'تحديث الأذونات',
+];
